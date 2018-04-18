59d1ce660da8df5f3010b34e1480868e11a08028
NoelleDL <noelle.legrain@gmail.com>
2018-04-18 11:25:35 +0100

    Added act_as_votable gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-
+ # /likes
+  def likes
+  end
   # GET /posts
   # GET /posts.json
   def index
